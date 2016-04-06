1b87f7ea0c411b5de75fdd0e7ab1cf9ef0867890
Priyam <priyam8parashar@gmail.com>
2016-04-06 01:18:44 -0400

    added HTN planner; doesn't return solution right now

--- test_planner_plugin.py
+++ test_planner_plugin.py
@@ -30,7 +30,7 @@ class TestPlannerPlugin(PluginBase):
         logger.info("adding atomic operators to pyhop:")
         # note: can call declare_operators multiple times for current situation
         hop.declare_operators(
-            self.forward,
+            self.move_forward,
             self.turn_left,
             self.turn_right,
             self.break_block)
@@ -41,34 +41,39 @@ class TestPlannerPlugin(PluginBase):
             'get_resource',
             self.find_route_to_resource,    # BFS() or A*
             self.navigate_to_resource,      # follow the path BFS spewed out
-            self.acquire_resource)          # break the block and move forward
+            self.acquire_resource)          # break the block and move move_forward
         hop.print_methods(hop.get_methods())
 
         # set state variables
         # will be used by operators and methods
         self.state = hop.State('start_state')
         self.state.start_loc = self.start_location
-        self.state.current_loc = self.state.start_loc
+        self.state.current_position = self.state.start_loc
         self.state.current_orientation = self.orientation
         self.state.goal_loc = self.gold_location
-        self.state.block = 1                # 1: unbroken, 0: broken
+        self.state.gold = 1                # 1: unbroken, 0: broken
+        self.state.path = []
+        self.state.gold_reached = 0
+        self.state.path_found = 0
+        self.state.path_idx = -1
+        self.state.gold_acquired = 0
 
-        ploader.provides('TestAtomicOperators',self)
+        ploader.provides('TestPlanner',self)
 
     #############
     # operators #
     #############
 
     # assuming location to be (x,y)
-    def forward(self):
+    def move_forward(self):
         if (self.state.current_orientation == 0):
-            self.state.current_loc[1] = self.state.current_loc[1] + 1
+            self.state.current_position[1] = self.state.current_position[1] + 1
         if (self.state.current_orientation == 1):
-            self.state.current_loc[1] = self.state.current_loc[0] + 1
+            self.state.current_position[1] = self.state.current_position[0] + 1
         if (self.state.current_orientation == 2):
-            self.state.current_loc[1] = self.state.current_loc[1] - 1
+            self.state.current_position[1] = self.state.current_position[1] - 1
         if (self.state.current_orientation == 3):
-            self.state.current_loc[1] = self.state.current_loc[0] - 1
+            self.state.current_position[1] = self.state.current_position[0] - 1
 
         return self.state
 
@@ -94,8 +99,112 @@ class TestPlannerPlugin(PluginBase):
         # will be used to call the hop.plan command
 
     def find_route_to_resource(self):
-        path = self.compute_path(self.state.current_loc, self.state.goal_loc)
-        return path
+        if not self.state.path:
+            self.state.path = self.testroom.compute_path(self.state.current_position, self.state.goal_loc)
+            if not self.state.path:
+                return False
+            else:
+                self.state.path_found = 1
+                self.state.path_length = len(self.state.path)
+                self.state.path_idx = 0
+        return None
 
-    def
+    def navigate_to_resource(self):
+        if self.state.path_found == 1 and self.state.gold_reached == 0:
+            next_position = self.state.path[self.state.path_idx]
+            if self.state.path_idx == (len(self.state.path) - 1):
+                self.state.gold_reached == 1
+                return False
+            if next_position[0]>self.state.current_position[0]:
+                if self.state.current_orientation == 1:
+                    self.state.path_idx = self.state.path_idx + 1
+                    return [('move_forward')]
+                elif self.state.current_orientation == 3:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 0:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 2:
+                    return [('turn_left')]
+            if next_position[0]<self.state.current_position[0]:
+                if self.state.current_orientation == 3:
+                    self.state.path_idx = self.state.path_idx + 1
+                    return [('move_forward')]
+                elif self.state.current_orientation == 2:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 1:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 0:
+                    return [('turn_left')]
+            if next_position[1]>self.state.current_position[1]:
+                if self.state.current_orientation == 0:
+                    self.state.path_idx = self.state.path_idx + 1
+                    return [('move_forward')]
+                elif self.state.current_orientation == 3:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 2:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 1:
+                    return [('turn_left')]
+            if next_position[1]<self.state.current_position[1]:
+                if self.state.current_orientation == 2:
+                    self.state.path_idx = self.state.path_idx + 1
+                    return [('move_forward')]
+                elif self.state.current_orientation == 1:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 0:
+                    return [('turn_right')]
+                elif self.state.current_orientation == 3:
+                    return [('turn_left')]
+        else:
+            return False
+
+    def acquire_resource(self):
+        if self.state.gold_reached == 1 and self.state.gold_acquired == 0:
+            if self.state.gold == 1:
+                #gold block is unbroken
+                #face the block, if already facing then break the block
+                if self.state.goal_loc[1]<self.state.current_position[1]:
+                    if self.state.current_orientation == 2:
+                        return [('break_block')]
+                    elif self.state.current_orientation == 1:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 0:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 3:
+                        return [('turn_left')]
+                if self.state.goal_loc[1]>self.state.current_position[1]:
+                    if self.state.current_orientation == 0:
+                        return [('break_block')]
+                    elif self.state.current_orientation == 3:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 2:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 1:
+                        return [('turn_left')]
+                if self.state.goal_loc[0]<self.state.current_position[0]:
+                    if self.state.current_orientation == 3:
+                        return [('break_block')]
+                    elif self.state.current_orientation == 1:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 2:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 0:
+                        return [('turn_left')]
+                if self.state.goal_loc[0]>self.state.current_position[0]:
+                    if self.state.current_orientation == 1:
+                        return [('break_block')]
+                    elif self.state.current_orientation == 3:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 0:
+                        return [('turn_right')]
+                    elif self.state.current_orientation == 2:
+                        return [('turn_left')]
+
+            else:
+                self.state.gold_acquired = 1
+                return [('move_forward')]
+        else:
+            return False
+
+
 
